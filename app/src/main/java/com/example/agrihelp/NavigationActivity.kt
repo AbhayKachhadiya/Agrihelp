fe6ee1132c8d51e7a31b0252bc4920ebcb2a7ae2
AbhayKachhadiya <kachhadiyaabhay22@gmail.com>
2023-01-19 12:38:39 +0530

    Logout Done

--- app/src/main/java/com/example/agrihelp/NavigationActivity.kt
+++ app/src/main/java/com/example/agrihelp/NavigationActivity.kt
@@ -1,8 +1,11 @@
 package com.example.agrihelp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavigationActivity : AppCompatActivity() {
@@ -10,43 +13,57 @@ class NavigationActivity : AppCompatActivity() {
     private lateinit var bottomNavView: BottomNavigationView
     private lateinit var homeFragment: HomeFragment
     private lateinit var contactUsFragment: ContactUsFragment
+    private lateinit var sharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
 
         bottomNavView = findViewById(R.id.bnNavigation)
-        bottomNavView.setOnItemSelectedListener (::bottomNavItemSelected)
+        bottomNavView.setOnItemSelectedListener(::bottomNavItemSelected)
 
         homeFragment = HomeFragment()
         contactUsFragment = ContactUsFragment()
 
         showHomeFragment()
-    }
 
-    private fun bottomNavItemSelected(item: MenuItem): Boolean {
-        when(item.itemId)
-        {
-            R.id.navHome -> showHomeFragment()
-            R.id.navContactUs -> showContactUsFragment()
-            else -> return false
-        }
-        return true
-    }
-
-    private fun showHomeFragment()
-    {
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.fragmemtContainer, homeFragment)
-            commit()
+        findViewById<ImageView>(R.id.icLogout).setOnClickListener{
+            logOut()
         }
     }
 
-    private fun showContactUsFragment()
-    {
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.fragmemtContainer, contactUsFragment)
-            commit()
-        }
+    private fun logOut() {
+
+        sharedPref = getSharedPreferences("agrihelp", MODE_PRIVATE)
+
+        val spEditor = sharedPref.edit()
+        spEditor.remove("Email")
+        spEditor.apply()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
+
+
+private fun bottomNavItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+        R.id.navHome -> showHomeFragment()
+        R.id.navContactUs -> showContactUsFragment()
+        else -> return false
+    }
+    return true
+}
+
+private fun showHomeFragment() {
+    supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fragmemtContainer, homeFragment)
+        commit()
+    }
+}
+
+private fun showContactUsFragment() {
+    supportFragmentManager.beginTransaction().apply {
+        replace(R.id.fragmemtContainer, contactUsFragment)
+        commit()
+    }
+}
 }
