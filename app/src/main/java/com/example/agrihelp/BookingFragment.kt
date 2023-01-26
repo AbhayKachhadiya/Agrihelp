5546c56cabd7d8be5ff4921ac06c126d47426259
Heer Gosrani <gosraniheer@gmail.com>
2023-01-26 22:51:42 +0530

    add booking fragment

--- app/src/main/java/com/example/agrihelp/BookingFragment.kt
+++ app/src/main/java/com/example/agrihelp/BookingFragment.kt
@@ -5,11 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.models.SlideModel
 
 
 class BookingFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_booking, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+        imageList.add(SlideModel(R.drawable.harvester))
+        imageList.add(SlideModel(R.drawable.drone_sprayer))
+        imageList.add(SlideModel(R.drawable.thresh))
+
+        val imageSlider = view.findViewById<ImageSlider>(R.id.image_slider)
+        imageSlider.setImageList(imageList)
     }
 }
